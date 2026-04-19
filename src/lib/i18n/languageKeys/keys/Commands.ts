c56bb1db772244fd749fdc504f356023a7d05322
Williams Bussat <williams@wbussat.fr>
2026-04-19 11:47:23 +0200

    feat: add birthday command translations

--- src/lib/i18n/languageKeys/keys/Commands.ts
+++ src/lib/i18n/languageKeys/keys/Commands.ts
@@ -1,2 +1,3 @@
+export * as Birthday from '#lib/i18n/languageKeys/keys/commands/Birthday';
 export * as Config from '#lib/i18n/languageKeys/keys/commands/Config';
 export * as Ping from '#lib/i18n/languageKeys/keys/commands/Ping';
